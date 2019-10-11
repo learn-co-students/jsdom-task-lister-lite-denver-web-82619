283c4424777984855217e5eedd8b9839e62234ea
Damon Self <damongself@gmail.com>
2019-10-11 10:29:41 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form = document.getElementById('create-task-form')
+  form.addEventListener("submit", event => {
+    event.preventDefault();
+    let input = document.getElementById('new-task-description')
+    let list = document.getElementById('list')
+    let li = document.createElement('li')
+
+    li.innerHTML = input.value
+    list.append(li)
+  })
 });
